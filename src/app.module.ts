6a8e8d7c193c948490c6191818e5a85c393e0fcc
Matheus <math.av7@outlook.com>
2023-06-26 21:17:57 -0300

    feature<log>: added `nestjs-pino` for pino built-in logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { LoggerModule } from 'nestjs-pino';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 3001;
+const logger = new Logger();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(PinoLogger));
+  app.flushLogs();
+  await app.listen(PORT, () => logger.log(`Server is running on port ${PORT}`));
 }
 bootstrap();
