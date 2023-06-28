46c59ef97534b18c381f10d2c0d6587cff2b86aa
Matheus <math.av7@outlook.com>
2023-06-28 00:14:36 -0300

    style: organized folders structure

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import { Logger } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './config/prisma/prisma.service';
 
 const PORT = process.env.PORT || 3001;
 const logger = new Logger();
