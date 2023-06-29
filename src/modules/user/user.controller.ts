ac4e4a6af2f68bc513015abe33a06a3e8613f392
Matheus <math.av7@outlook.com>
2023-06-29 19:14:06 -0300

    WIP: user CRUD and validation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,13 +1,49 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { UserService } from "./user.service";
-import { CreateUserDto, UserResponse } from "./dtos";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, UpdateUserDto, UserResponse } from './dtos';
+import { UserPermissionPipe } from 'src/common/pipes';
 
 @Controller('api/user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
-    @Post()
-    public async create(@Body() input: CreateUserDto): Promise<UserResponse> {
-        return await this.userService.create(input)
-    }
+  @Post()
+  public async create(@Body() input: CreateUserDto): Promise<UserResponse> {
+    return await this.userService.create(input);
+  }
+
+  @Get()
+  public async findAll(): Promise<UserResponse[]> {
+    return await this.userService.findAll();
+  }
+
+  @Get('by-id-or-email/:id')
+  public async findByIdOrEmail(@Param('id') id: string): Promise<UserResponse> {
+    return await this.userService.findByIdOrEmail(id);
+  }
+
+  @Delete(':id')
+  public async delete(@Param('id') id: string): Promise<void> {
+    await this.userService.delete(id);
+  }
+
+  @Patch(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async update(
+    @Body() input: UpdateUserDto,
+    @Param('id') id: string,
+  ): Promise<UserResponse> {
+    return await this.userService.update(id, input);
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,20 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { hash, hashSync } from 'bcrypt';
 import { plainToInstance } from 'class-transformer';
-import { CreateUserDto, UserResponse } from './dtos';
+import { CreateUserDto, UpdateUserDto, UserResponse } from './dtos';
 import { UserRepository } from './user.repository';
-import { hashSync } from 'bcrypt';
-
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  public async create(input: CreateUserDto): Promise<UserResponse> {
+    const hashedPassword = await this.hashPassword(input.password);
+    const user = { password: hashedPassword, ...input };
+
+    const result = await this.userRepository.create(user);
+
+    return plainToInstance(UserResponse, result);
   }
 
-  public async create(input:CreateUserDto): Promise<UserResponse> {
-    input.password = hashSync(input.password, 10)
+  public async findAll(): Promise<UserResponse[]> {
+    const result = await this.userRepository.findMany();
 
-    const result = this.userRepository.create(input)
+    return plainToInstance(UserResponse, result);
+  }
 
-   return plainToInstance(UserResponse, result)
+  public async findByIdOrEmail(input: string): Promise<UserResponse> {
+    const result = await this.userRepository.findByIdOrEmail(input);
+
+    return plainToInstance(UserResponse, result);
+  }
+
+  public async delete(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+
+  public async update(id: string, input: UpdateUserDto): Promise<UserResponse> {
+    const result = await this.userRepository.update(id, input);
+
+    return plainToInstance(UserResponse, result);
+  }
+
+  async hashPassword(password: string): Promise<String> {
+    const saltRounds = 10;
+    return await hash(password, saltRounds);
   }
 }
