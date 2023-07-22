56be53f3007b47a5bcc187258260b0f7f078e01f
Matheus <math.av7@outlook.com>
2023-07-21 23:27:12 -0300

    refactor: omit properties from `CreateUserDto`

--- src/modules/project/dtos/update-project.dto.ts
+++ src/modules/project/dtos/update-project.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreateProjectDto } from "./create-project.dto";
+import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { CreateProjectDto } from './create-project.dto';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends OmitType(CreateProjectDto, [
+  'user_id',
+] as const) {}
