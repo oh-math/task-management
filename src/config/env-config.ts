308f6eef724549112c65d7e056a0465cc83c09c0
Matheus <math.av7@outlook.com>
2023-07-27 22:32:10 -0300

    refactor: isolating config dotenv logic to `.env-config.ts`

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -1,11 +1,16 @@
+import { config } from 'dotenv';
 import { join } from 'path';
 
-const NODE_ENV = process.env.NODE_ENV;
-const rootDir = process.cwd();
+const configureDotenv = () => {
+  const NODE_ENV = process.env.NODE_ENV;
+  const rootDir = process.cwd();
 
-let envPath: string;
+  let envPath: string;
 
-if (NODE_ENV === 'dev') envPath = join(rootDir, '/.env.dev');
-else if (NODE_ENV === 'prod') envPath = join(rootDir, '/.env.prod');
+  if (NODE_ENV === 'dev') envPath = join(rootDir, '/.env.dev');
+  else if (NODE_ENV === 'prod') envPath = join(rootDir, '/.env.prod');
 
-export default envPath;
+  envPath && config({ path: envPath });
+};
+
+export default configureDotenv;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { config } from 'dotenv';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import { AppModule } from './app.module';
-import envPath from './config/env-config';
 import { PrismaService } from './config/prisma/prisma.service';
+import configureDotenv from './config/env-config';
 
-envPath && config({ path: envPath });
+configureDotenv()
 
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 const PORT = process.env.PORT || 3001;
