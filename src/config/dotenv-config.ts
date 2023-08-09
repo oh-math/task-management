cdb2e3a9ff46f79261ec0ba5de3cdbf8b8f02bf6
Matheus <math.av7@outlook.com>
2023-08-08 23:02:47 -0300

    refactor: rename with more significant file name

--- src/config/dotenv-config.ts
+++ src/config/dotenv-config.ts
@@ -1,20 +1,21 @@
 import { config } from 'dotenv';
 import { join } from 'path';
 
-const NODE_ENV = process.env.NODE_ENV;
-const rootDir = process.cwd();
-
 function getDotenvPath(): string | undefined {
+  const NODE_ENV = process.env.NODE_ENV;
+  const rootDir = process.cwd();
+
   if (NODE_ENV === 'dev') return join(rootDir, '/.env.dev');
   if (NODE_ENV === 'prod') return join(rootDir, '/.env.prod');
 }
 
 function configureDotenvPath(): void {
   const dotenvPath = getDotenvPath();
-  
+
   if (dotenvPath) {
     config({ path: dotenvPath });
   }
 }
 
-export default configureDotenvPath;
+export { configureDotenvPath, getDotenvPath };
+
